bfd1963190302460b7aaec0d6c5e5453c9c08d06
Douglas <douglas_champion@hotmail.com>
2018-03-25 18:52:38 -0300

    Atualiza show.blade.php (Professores não podem editar as mensagens dos alunos, apenas apaga-las)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,13 +10,15 @@
     <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
     <hr>
     @if (!Auth::guest())
+        @if (Auth::user()->id == $post->user_id)
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+        @endif
         @if (Auth::user()->id == $post->user_id || (Auth::user()->type == 1 && ($user->type != 1 || $user->id == Auth::user()->id)))
-        <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
-        {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
-        {{Form::hidden('_method', 'DELETE')}}
-        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+            {{Form::hidden('_method', 'DELETE')}}
+            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
         
-        {!!Form::close()!!}
+            {!!Form::close()!!}
         @endif
     @endif
 @endsection
