82de97141d92e679694decad9c136890913f9b14
Gabes <contato@gabesdev.com.br>
2018-03-26 15:30:29 -0300

    Altera a página do post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,16 +9,16 @@
 </div>
     <div>{!!$post->body!!}</div>
     <hr>
-    <small>Escrito em {{$post->created_at}} às {{$post->user->name}}</small>
+    <small>Escrito em {{ strftime('%d/%m/%Y', strtotime($post->created_at))}} por: {{$post->user->name}}</small>
     <hr>
     @if (!Auth::guest())
         @if (Auth::user()->id == $post->user_id)
-            <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Editar</a>
         @endif
         @if (Auth::user()->id == $post->user_id || (Auth::user()->type == 1 && ($user->type != 1 || $user->id == Auth::user()->id)))
             {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
             {{Form::hidden('_method', 'DELETE')}}
-            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            {{Form::submit('Deletar', ['class' => 'btn btn-danger'])}}
         
             {!!Form::close()!!}
         @endif
