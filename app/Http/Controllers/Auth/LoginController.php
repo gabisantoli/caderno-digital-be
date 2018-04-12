7fc75d15c0462c45aee1f29b93d8cda037adb7f7
Gabes <contato@gabesdev.com.br>
2018-04-11 22:29:03 -0300

    Bugfixes, logout separado de web e api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-use Illuminate\Http\Request;
+use Auth;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -30,6 +31,25 @@ class LoginController extends Controller
     }
 
     public function logout(Request $request){
+        Auth::logout();
+        return view('pages.index');    
+    }
+
+    public function apiLogin(Request $request){
+        $this->validateLogin($request);
+        
+        if($this->attemptLogin($request)){
+            $user = $this->guard()->user();
+            $user->generateToken();
+
+            return response()->json([
+                'data' => $user->toArray(),
+            ]);
+        }
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    public function apiLogout(Request $request){
         $user = Auth::guard('api')->user();
         if($user){
             $user->api_token = null;
