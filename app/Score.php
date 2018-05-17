9a8b0d497f32339e0a8b750c67c743cd2cdefb44
TheeDouglasAM3 <douglas_champion@hotmail.com>
2018-05-16 21:30:36 -0300

    Cria método para calcular pontos no model Score

--- app/Score.php
+++ app/Score.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
-    //
+    
+    public function updateScore($user_id, $pontos, $ganhar_pontos=true){
+        $score = Score::where("user_id", $user_id)->take(1)->get()[0];
+        if($ganhar_pontos){
+            $score->points += $pontos;
+        }else{
+            $score->points -= $pontos;
+        }
+        $score->level = ManageUserLevel::checkLevel($score->points);
+        $score->save();
+    }
 }
