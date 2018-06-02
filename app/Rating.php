90052c093738d6db032c0812e742b268f4f7b18a
Douglas Alves Marcelino <douglas_champion@hotmail.com>
2018-06-02 15:57:51 -0300

    Documenta funções do model Rating

--- app/Rating.php
+++ app/Rating.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
+    /**
+     * Retorna o número de votos positivos e negativos de uma postagem ou resposta
+     * @param Integer $id - id de um post ou answer
+     * @param String $context - contexto pode ser 'post' ou  'answer'
+     * @return Array $num_rating - array com o número de votos positivos e negativos
+     */
     public function getRatingFromContext($id, $context){
+        
         $rating = new Rating();
         $num_rating = array();
 
@@ -24,7 +31,15 @@ class Rating extends Model
         return $num_rating;
     }
 
+    /**
+     * Atualiza o Score de um usuário após uma avaliação de postagem ou resposta
+     * @param Object $rating - um objeto do tipo Rating
+     * @param Integer $points - quantidade de pontos a ser recebida ou retirada
+     * @param Boolean $reverse - inverte a lógica de soma e subtração de pontos
+     * (ideal para quando o usuário quer remover uma avaliação)
+     */
     public function calculateScore($rating, $points, $reverse=false){
+        
         $score = new Score();
         $user_id = -1;
 
@@ -47,19 +62,35 @@ class Rating extends Model
             if(!$reverse) $score->updateScore($user_id, $points, false);
             if($reverse) $score->updateScore($user_id, $points);
         }
-
     }
 
+    /**
+     * Verifica se o usuário já votou em uma postagem ou resposta em especifica
+     * @param Object $rating - objeto do tipo Rating
+     * @return Array - retorna um array Rating do voto do usuário
+     */
     public function userAlreadyRated($rating){
+        
         $condition = [
             "id_user" => $rating->id_user,
             "id_context" => $rating->id_context,
             "context" => $rating->context
         ];
+        
         return $rating::where($condition)->get()->toArray();
     }
 
+    /**
+     * Adiciona uma classe CSS nos ícones de votação, caso o usuário tenha avaliado
+     * uma postagem ou resposta
+     * @param Integer $id_user - id do usuário
+     * @param Integer $id_context - id de um post ou answer
+     * @param String $context - context pode ser 'post' ou 'answer'
+     * @param Object $context_obj - um objeto do tipo Post ou Answer
+     * @return Object $context_obj - o mesmo objeto do parametro $context_obj
+     */
     public function addClassCSSToVotingIcon($id_user, $id_context, $context, $context_obj){
+        
         $rating_context = new Rating();
         $rating_context->id_user = $id_user;
         $rating_context->id_context = $id_context;
@@ -67,15 +98,12 @@ class Rating extends Model
         $voto_usuario = $this->userAlreadyRated($rating_context);
 
         if(sizeof($voto_usuario) != 0){
-            
             if($voto_usuario[0]['status'] == "positivo"){
                 $context_obj->flag_voto_usuario['positivo_css'] = "voto-positivo";
             }
-
             if($voto_usuario[0]['status'] == "negativo"){
                 $context_obj->flag_voto_usuario['negativo_css'] = "voto-negativo";
-            }
-                
+            }       
         }
 
         return $context_obj;
