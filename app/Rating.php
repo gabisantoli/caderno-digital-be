8fbf6245e7532a635122a46b41e1a9ef5c54215e
Douglas Alves Marcelino <douglas_champion@hotmail.com>
2018-06-01 23:48:26 -0300

    Cria model Rating + função para retornar número de avaliações pos e negs

---
+++ app/Rating.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    public function getRatingFromContext($id, $context){
+        $rating = new Rating();
+        $num_rating = array();
+
+        $condition = [
+            "id_context" => $id,
+            "context" => $context,
+            "status" => "positivo"
+        ];
+        $num_rating['positivo'] = sizeof($rating::where($condition)->get()->toArray());
+
+        $condition = [
+            "id_context" => $id,
+            "context" => $context,
+            "status" => "negativo"
+        ];
+        $num_rating['negativo'] = sizeof($rating::where($condition)->get()->toArray());
+
+        return $num_rating;
+    }
+}
